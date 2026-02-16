801878bfe34b69b32c4d147de8b87c50cbe27cbf
danielcramirez <daniel00250@hotmail.com>
2026-02-16 16:37:40 -0500

    Reorganize: Move infrastructure to app/ and database/ folders - Clean MVC structure

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../db.php';
-require_once __DIR__ . '/../helpers.php';
-require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/../app/config.php';
+require_once __DIR__ . '/../app/db.php';
+require_once __DIR__ . '/../app/helpers.php';
+require_once __DIR__ . '/../app/auth.php';
 require_once __DIR__ . '/view.php';
